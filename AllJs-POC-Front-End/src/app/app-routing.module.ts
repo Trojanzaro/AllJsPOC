75bd0f2c0f5a11f917d0bce30ff483a7b4019470
maria-fakou <60821565+maria-fakou@users.noreply.github.com>
2020-04-03 19:40:51 +0300

    created login and signup forms

--- AllJs-POC-Front-End/src/app/app-routing.module.ts
+++ AllJs-POC-Front-End/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component'; 
+import { SignupFormComponent} from './signup-form/signup-form.component';
 
+const routes: Routes = [
+{
+	path:'login-form',
+	component: LoginFormComponent
+}
+,
+{
+	path:'signup-form',
+	component: SignupFormComponent
 
-const routes: Routes = [];
+},
+{
+	path:'',
+	component: LoginFormComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ AllJs-POC-Front-End/src/app/login-form/login-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {User} from '../userClass';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+	loginForm: FormGroup;
+
+  constructor(private router: Router) { }
+
+  users: User[];
+
+  ngOnInit(){
+  	this.loginForm = new FormGroup({
+          username: new FormControl(''),
+          password: new FormControl('')
+      
+    });
+  }
+
+  onSubmit(){
+    //this.users.find( (User)=>  == this.loginForm.get('username'));
+
+  }
+
+
+
+}
