928fc21720bf5c346076bfaad4cae396d4d83e8f
Trojanzaro <anastasiszaro@gmail.com>
2020-04-08 12:33:31 +0300

    Finished refactoring to mongoose

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Include Everything
-var mongo = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 var express = require('express');
 var cors = require('cors')
@@ -20,12 +19,11 @@ var usersSchema = mongoose.Schema({
     firstname: String,
     lastname: String,
     birthDate: String,
-	username: String,
-	password: String,
-	email: String
+    username: String,
+    password: String,
+    email: String
 })
-
-var User =  mongoose.model("User", usersSchema);
+var User = mongoose.model("User", usersSchema);
 
 //Add a POST request handler to express to handle login
 app.post('/users/login', async function (req, res) {
@@ -33,17 +31,15 @@ app.post('/users/login', async function (req, res) {
     var un = req.body.username;
     var pass = req.body.password;
     await mongoose.connect('mongodb://localhost:27017/AlljsPOCDB', { useNewUrlParser: true });
-    let user = await User.findOne({username: un, password: pass});
+    let user = await User.findOne({ username: un, password: pass });
     if (user === null) {
         res.status(404);
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({
+        res.send({
             errorCode: 404,
             errorMessage: "Username or Password wrong!"
-        }));
+        });
         res.end();
     } else {
-        res.setHeader('Content-Type', 'application/json');
         res.send({
             username: user.username,
             email: user.email
@@ -55,52 +51,36 @@ app.post('/users/login', async function (req, res) {
 }); //End of CallBack Hell
 
 
-app.post('/users/signup', function (req, res){
+app.post('/users/signup', async function (req, res) {
 
-    var fn = req.body.firstname;
-    var ln = req.body.lastname;
-    var bd = req.body.birthdate;
-    var em = req.body.email;
-    var un = req.body.username;
-    var pass = req.body.password;
+    var user = {
+        firstname: req.body.firstname,
+        birthdate: req.body.birthdate,
+        email: req.body.email,
+        username: req.body.firstname,
+        password: req.body.password,
+    };
 
-    mongo.connect(url, function(err, db){
-        if(err) throw err;
-        var dbo = db.db("AlljsPOCDB");
-        var query = {
-            firstname : fn,
-            lastname : ln,
-            birthdate : bd,
-            email : em,
-            username : un,
-            password : pass
-        };
-        //insert new user
-        dbo.collection("users").insertOne(query, function(err, result, docsInserted){
-            if (err) throw err;
+    await mongoose.connect('mongodb://localhost:27017/AlljsPOCDB', { useNewUrlParser: true });
+    let result = await User.create(user);
 
-            if(result.length === 0 ){
-                res.status(404);
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify({
-                    errorCode: 404,
-                    errorMessage: "Something went wrong, try again!"
-                }));
-                res.end();
-            }else {
-                res.setHeader('Content-Type', 'application/json');
-                res.send({
-                    insertedCount: result.insertedCount,
-                    insertedId: result.insertedId,
-                    n: result.result.n,
-                    ok : result.result.ok
-                });
-                res.end();
-            }
-            //console.log("Item inserted");
-            db.close();
-        });        
-    });
+    if (result.length === 0) {
+        res.status(404);
+        res.send({
+            errorCode: 404,
+            errorMessage: "Something went wrong, try again!"
+        });
+        res.end();
+    } else {
+        console.log(result);
+        res.setHeader('Content-Type', 'application/json');
+        res.send({
+            insertedId: result._id,
+            ok: 'OK!'
+        });
+        res.end();
+    }
+    await mongoose.disconnect();
 });
 
 var server = app.listen(9090, function () {
