9070a58cf180c6cec5144e7186cd90745965643c
Trojanzaro <anastasiszaro@gmail.com>
2020-04-10 13:14:44 +0300

    Added Authorization through Cognito user pool

--- AllJs-POC-Front-End/src/app/signup-form/signup-form.component.ts
+++ AllJs-POC-Front-End/src/app/signup-form/signup-form.component.ts
@@ -4,18 +4,20 @@ import { Router } from '@angular/router';
 import { NewUser } from '../userClass';
 import { UserService } from '../user.service';
 
+import { Auth } from 'aws-amplify';
+// tslint:disable-next-line: indent
 @Component({
-	selector: 'app-signup-form',
-	templateUrl: './signup-form.component.html',
-	styleUrls: ['./signup-form.component.css']
+    selector: 'app-signup-form',
+    templateUrl: './signup-form.component.html',
+    styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent implements OnInit {
-	form: FormGroup;
-	newUser: NewUser;
-	errorOnSignUp = {
-		error: false,
-		errorPhrase: ''
-	  };
+    form: FormGroup;
+    newUser: NewUser;
+    errorOnSignUp = {
+        error: false,
+        errorPhrase: ''
+	};
 	constructor(
 		private router: Router,
 		private userService: UserService
@@ -23,32 +25,38 @@ export class SignupFormComponent implements OnInit {
 
 	ngOnInit() {
 		this.form = new FormGroup({
-			firstname: new FormControl(''),
-			lastname: new FormControl(''),
-			birthDate: new FormControl(''),
+			email: new FormControl(''),
+			phone: new FormControl(''),
 			username: new FormControl(''),
-			password: new FormControl(''),
-			remember: new FormControl('')
+			password: new FormControl('')
 		});
 	}
 
-	onSubmit() {
-		const user = {
-			firstname: this.form.value.username,
-			lastname: this.form.value.lastname,
-			birthDate: this.form.value.birthDate,
-			username: this.form.value.username,
-			password: this.form.value.password,
-			remember: this.form.value.remember,
-		}
-		this.userService.signUp(user)
-			.subscribe((data) => {
-				this.router.navigate(['/']);
+	async onSubmit() {
+		let email = this.form.value.email;
+		let phone_number = this.form.value.phone;
+		let username = this.form.value.username;
+		let password = this.form.value.password;
+		console.log(username);
+		Auth.signUp({
+			username,
+			password,
+			attributes: {
+				email,          // optional
+				phone_number,   // optional - E.164 number convention
+				// other custom attributes
 			},
-			(err) => {
-				this.errorOnSignUp.error = true;
-				this.errorOnSignUp.errorPhrase = err.error.errorMessage;
+			validationData: []  // optional
+		})
+			.then(data => { 
+				console.log(data);
+				this.router.navigate(['/']);
 			})
+			.catch(err => {
+				console.log(err);
+				this.errorOnSignUp.error = true;
+				this.errorOnSignUp.errorPhrase = err.message;
+			});
 		console.log(this.form.value);
 	}
 }
